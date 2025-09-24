03f745ead3c71d7f98e2d000c64c9acbe3ff7142
John R <johnrami0310@gmail.com>
2025-09-24 16:28:43 -0500

    Added array lists of images needed for the Home Page

--- app_v2.js
+++ app_v2.js
@@ -7,6 +7,49 @@
 
 // Home Page JS
 
+    // Image Arrays
+    const primaryMissions = [
+        "PM_burden-of-trust.svg",
+        "PM_hidden-supplies.svg",
+        "PM_linchpin.svg",
+        "PM_purge-the-foe.svg",
+        "PM_scorched-earth.svg",
+        "PM_supply-drop.svg",
+        "PM_take-and-hold.svg",
+        "PM_terraform.svg",
+        "PM_the-ritual.svg",
+        "PM_unexploded-ordinance.svg"
+    ];
+    const terrainLayouts = [
+        "terrain_layouts/layout1.png",
+        "terrain_layouts/layout2.png",
+        "terrain_layouts/layout3.png",
+        "terrain_layouts/layout4.png",
+        "terrain_layouts/layout5.png",
+        "terrain_layouts/layout6.png",
+        "terrain_layouts/layout7.png",
+        "terrain_layouts/layout8.png"
+    ];
+    const deployZones = [
+        "deployment_zones/DZ_crucible-of-battle.svg",
+        "deployment_zones/DZ_dawn-of-war.svg", 
+        "deployment_zones/DZ_hammer-and-anvil.svg", 
+        "deployment_zones/DZ_search-and-destroy.svg", 
+        "deployment_zones/DZ_sweeping-engagement.svg",
+        "deployment_zones/DZ_tipping-point.svg"
+    ];
+    const twistList = [
+        "twist-rules/Tw_adapt-or-die.svg",
+        "twist-rules/Tw_bloodlust.svg",
+        "twist-rules/Tw_high-octane.svg",
+        "twist-rules/Tw_lords-of-war.svg",
+        "twist-rules/Tw_martial-pride.svg",
+        "twist-rules/Tw_night-fighting.svg",
+        "twist-rules/Tw_point-blank.svg",
+        "twist-rules/Tw_rapid-escalation.svg",
+        "twist-rules/Tw_ruinscape.svg"
+    ]
+
 
 
 
