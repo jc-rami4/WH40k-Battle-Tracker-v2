159cabb7fee01b378c12ee6a5a2a31e1be56dc8e
jc-rami4 <johnrami0310@gmail.com>
2025-09-24 15:45:15 -0500

    initial commit

---
+++ app_v2.js
@@ -0,0 +1,17 @@
+// JS Code Improved (Cause I sure as hell did NOT make that last one well)
+// First, we start with the home page
+// The JS needed for this will just be changing image types
+// There will be a need for randomizer
+
+
+
+// Home Page JS
+
+
+
+
+// Rounds JS
+
+
+
+
